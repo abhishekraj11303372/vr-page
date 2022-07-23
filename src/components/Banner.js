b09fe7c1937100eb797101a4e6f03d86fbc4eee6
Abhishek Raj <abhishekraj1130@gmail.com>
2022-07-23 20:11:25 +0530

    inline css added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,23 +5,25 @@ import User from './Users'
 import Img from '../assets/img/banner-img.png'
 
 const Banner = () => {
-  return <section>
+  return (
+  <section className='min-h-[600px] pt-24 pb-12 text-center relative lg:pt-48 lg:pb-0 lg:text-left'>
     <div className='container mx-auto'>
-      <div>
-        <div><h1>Let's Eplore <br/> Three-Dimension Visually</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut itaque modi ea maxime inventore doloremque? Numquam ipsa id pariatur consectetur dolor inventore illum, voluptatem sit optio tenetur minus illo necessitatibus?</p>
-        </div>
-        <div>image</div>
+      <div className='flex flex-col lg:flex-row'>
         <div>
-          <button>Get it now</button>
-          <a href='#'> Explore Device</a>
+          <h1 className='text-3xl font-bold mb-8 lg:text-5xl lg:leading-snug'>Let's Eplore <br/> Three-Dimension Visually</h1>
+          <p className='font-secondary mb-12 max-w-[440px] mx-auto lg:mx-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut itaque modi ea maxime inventore doloremque? Numquam ipsa id pariatur consectetur dolor inventore illum, voluptatem sit optio tenetur minus illo necessitatibus? </p>
+          <div className='flex items-center justify-around spacce-x-4 max-w-[320px] mx-auto mb-12 lg:space-x-8 lg:mx-0 lg-max-w-none'>
+            <button className='btn'>Get it now</button>
+            <a href='#'>Explore Device</a>
+          </div>
         </div>
         <div>
           <img src={Img} />
         </div>
       </div>
     </div>
-  </section>;
+  </section>
+  );
 };
 
 export default Banner;
