7f6a329486bd85eda4f1ecde86e98eeec4505c41
Abhishek Raj <abhishekraj1130@gmail.com>
2022-07-29 15:25:40 +0530

    Testimonial added

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import TestimonialSlider from '../components/TestimonialSlider'
+
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <section className='mb-64 lg:mb-48'>
+      <div className='container mx-auto'>
+        <div className='bg-purple-400/10 max-h-[400px] p-12 rounded-lg'>
+          <div className='text-center'> 
+            <h2 className='text-3xl font-bold mb-6'>What our clients say</h2>
+            <p className='max-w-2xl mx-auto mb-12 lg:mb-24'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, deserunt! Nemo officiis doloremque, ipsa accusantium ullam odio. Blanditiis nostrum ut labore, nobis autem rem alias accusamus. Distinctio recusandae quibusdam consectetur?</p>
+          </div>
+          <div className='bg-pink'>
+            <TestimonialSlider />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonial;
